6a26eca74bfaaeb23e3e64de916a3503039f64ec
linnykoleh <oleh.linnyk@megogo.net>
2024-09-15 11:08:21 +0300

    Initial commit

---
+++ src/main/java/demo/balancingdemo/BalancingDemoApplication.java
@@ -0,0 +1,13 @@
+package demo.balancingdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BalancingDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BalancingDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/demo/balancingdemo/BalancingDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.balancingdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BalancingDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
