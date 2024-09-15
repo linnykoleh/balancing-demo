9bd65f25d096a6b846d87c654068289abe186802
linnykoleh <oleh.linnyk@megogo.net>
2024-09-15 11:08:49 +0300

    Initial commit

--- src/main/java/demo/balancingdemo/BalancingDemoApplication.java
+++ src/main/java/demo/balancingdemo/BalancingDemoApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BalancingDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BalancingDemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BalancingDemoApplication.class, args);
+    }
 
 }
